208787726246682b7c5d3574a78dbad25f1ad4a6
mestihudson <mestihudson@gmail.com>
2020-03-31 01:46:40 -0300

    Adiciona menu

---
+++ ui/src/components/Menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1><a href='#' data-trigger='Home' @click='home'>Personal Stalker</a></h1>
+    <ul>
+      <li>
+        <button data-trigger='CompletedTasks' @click='completeds'
+        >Finalizadas</button>
+      </li>
+      <li>
+        <button data-trigger='Tasks' @click='tasks'
+        >Tarefas</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    completeds () {
+      this.$router.push({ name: 'completeds' })
+    },
+    tasks () {
+      this.$router.push({ name: 'tasks' })
+    },
+    home () {
+      this.$router.push({ name: 'home' })
+    }
+  }
+}
+</script>
