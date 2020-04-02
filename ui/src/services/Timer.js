f8f76250daf1770f768c06a7cf03c908dfd6af8e
mestihudson <mestihudson@gmail.com>
2020-04-02 02:16:57 -0300

    Adiciona um parâmetro de incremento no serviço Timer.ellipsed

--- ui/src/services/Timer.js
+++ ui/src/services/Timer.js
@@ -9,9 +9,9 @@ const timeOf = (date) => moment(date, DATETIME)
 
 const diffOf = (a, b, as) => timeOf(a).diff(timeOf(b), as)
 
-const format = (a, b) => {
+const format = (a, b, increment) => {
   const start = timeOf(b)
-  const end = timeOf(a).add(1, 'seconds')
+  const end = timeOf(a).add(increment, 'seconds')
   const days = moment.duration(diffOf(end, start, 'days'), 'days').asDays()
   const rest = moment().startOf('days').seconds(diffOf(end, start, 'seconds'))
     .format(TIME)
@@ -19,8 +19,8 @@ const format = (a, b) => {
 }
 
 export default {
-  ellipsed (time) {
+  ellipsed (time, increment = 1) {
     const now = Now.get()
-    return format(now, time || now)
+    return format(now, time || now, increment)
   }
 }
--- ui/tests/unit/services/Timer.spec.js
+++ ui/tests/unit/services/Timer.spec.js
@@ -4,8 +4,9 @@ import Now from '@/services/Now'
 import Timer from '@/services/Timer'
 
 describe('@/services/Timer', () => {
+  const NOW = '2020-03-28 17:00:00'
+
   beforeEach(() => {
-    const NOW = '2020-03-28 17:00:00'
     Now.get = jest.fn().mockImplementation(() => {
       return NOW
     })
@@ -15,10 +16,27 @@ describe('@/services/Timer', () => {
     jest.resetAllMocks()
   })
 
-  it('deve incrementar a data em 1 segundo', () => {
-    expect(Timer.ellipsed()).toBe('0:00:00:01')
-    expect(Timer.ellipsed('2020-03-28 16:59:00')).toBe('0:00:01:01')
-    expect(Timer.ellipsed('2020-03-27 17:00:00')).toBe('1:00:00:01')
-    expect(Timer.ellipsed('2019-03-28 17:00:00')).toBe('366:00:00:01')
+  it.each([
+    [NOW, NOW, 1, undefined, undefined, '0:00:00:01'],
+    [
+      '2020-03-28 16:59:00', NOW, 1, '2020-03-28 16:59:00', undefined,
+      '0:00:01:01'
+    ],
+    [
+      '2020-03-27 17:00:00', NOW, 1, '2020-03-27 17:00:00', undefined,
+      '1:00:00:01'
+    ],
+    [
+      '2019-03-28 17:00:00', NOW, 1, '2019-03-28 17:00:00', undefined,
+      '366:00:00:01'
+    ],
+    [
+      '2019-03-28 17:00:00', NOW, 36, '2019-03-28 17:00:00', 36,
+      '366:00:00:36'
+    ]
+  ])('deve incrementar o intervalo entre %p e %p em %p segundo(s)', (
+    timeDesc, reference, incrementDesc, time, increment, assert
+  ) => {
+    expect(Timer.ellipsed(time, increment)).toBe(assert)
   })
 })
